a1e8c3f14bd593404f15a7a956342f02ecb6f165
David Anderson <dave@natulte.net>
2022-02-13 01:28:17 -0800

    Fix up flake.nix a bit.
    Signed-off-by: David Anderson <dave@natulte.net>

--- flake.nix
+++ flake.nix
@@ -5,17 +5,9 @@
     flake-utils.lib.eachDefaultSystem
       (system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        pkg = let
-          rev = "673d93f27161b345e632b86754447973410ad121";
-        in pkgs.buildGo117Module rec {
-          pname = "livemon";
-          version = rev;
-          src = pkgs.fetchFromGitHub {
-            owner = "danderson";
-            repo = "livemon";
-            rev = rev;
-            sha256 = "sha256-W2xkSH62Ayg66iVeer03CgyRSh2gzVWFPWFmY0P3owg=";
-          };
+        pkg = pkgs.buildGo117Module rec {
+          name = "livemon";
+          src = ./.;
           vendorSha256 = "sha256-fdDPTvhdpd4mL04OikkO2+5csyE3o8VB/Ih/m2UGaiw=";
           postInstall = ''
             sed -i -e "s#/usr/bin#$out/bin#" livemon.service
