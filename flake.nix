80d29fa6394c9a1d91db7fb5c21ab19908189a64
David Anderson <dave@natulte.net>
2022-12-24 20:06:50 -0800

    rewrite flake.nix in an attempt to make it less irritating somehow.

--- flake.nix
+++ flake.nix
@@ -3,60 +3,64 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: let
-    mkPkg = pkgs: pkgs.buildGo119Module rec {
+    livemon = pkgs: pkgs.buildGo119Module rec {
       name = "livemon";
       src = ./.;
-      vendorSha256 = "sha256-fdDPTvhdpd4mL04OikkO2+5csyE3o8VB/Ih/m2UGaiw=";
+      vendorSha256 = "sha256-++SNlauqfk3RkwlocO3Yc7Hl/fxscLWwlMIL906A/n4=";
       postInstall = ''
-        sed -i -e "s#/usr/bin#$out/bin#" livemon.service
-        sed -i -e "/ListenStream/d" livemon.socket
-        install -D -m0444 -t $out/lib/systemd/system livemon.service livemon.socket
+         sed -i -e "s#/usr/bin#$out/bin#" livemon.service
+         sed -i -e "/ListenStream/d" livemon.socket
+         install -D -m0444 -t $out/lib/systemd/system livemon.service livemon.socket
       '';
     };
-  in
-    (flake-utils.lib.eachDefaultSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pkg = mkPkg pkgs;
-      in {
-        packages.livemon = pkg;
-        defaultPackage = pkg;
-      })) // {
-        nixosModules.livemon = { config, lib, pkgs, ... }: let
-          cfg = config.services.livemon;
-        in {
-          options.services.livemon = {
-            enable = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-            };
-            listenAddr = lib.mkOption {
-              type = lib.types.str;
-              default = "[::1]";
-            };
-            listenPort = lib.mkOption {
-              type = lib.types.port;
-              default = 9843;
-            };
-            monitoredServices = lib.mkOption {
-              type = lib.types.attrsOf lib.types.str;
-              default = {};
-            };
-          };
 
-          config = lib.mkIf cfg.enable {
-            systemd.packages = [ (mkPkg pkgs) ];
-            systemd.sockets.livemon = {
-              wantedBy = ["multi-user.target"];
-              listenStreams = [
-                "${cfg.listenAddr}:${builtins.toString cfg.listenPort}"
-                "/run/livemon/livemon.sock"
-              ];
-              unitConfig = {
-                PropagatesStopTo = "livemon.service";
-              };
-            };
+    perSystem = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkg = livemon pkgs;
+    in {
+      packages.livemon = pkg;
+      defaultPackage = pkg;
+    });
+
+    nixos = { config, lib, pkgs, ... }: let
+      cfg = config.services.livemon;
+    in {
+      options.services.livemon = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        listenAddr = lib.mkOption {
+          type = lib.types.str;
+          default = "[::1]";
+        };
+        listenPort = lib.mkOption {
+          type = lib.types.port;
+          default = 9843;
+        };
+        monitoredServices = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          default = {};
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        systemd.packages = [ (livemon pkgs) ];
+        systemd.sockets.livemon = {
+          wantedBy = ["multi-user.target"];
+          listenStreams = [
+            "${cfg.listenAddr}:${builtins.toString cfg.listenPort}"
+            "/run/livemon/livemon.sock"
+          ];
+          unitConfig = {
+            PropagatesStopTo = "livemon.service";
           };
         };
       };
+    };
+  in
+    perSystem // {
+      nixosModules.livemon = nixos;
+      nixosModules.default = nixos;
+    };
 }
